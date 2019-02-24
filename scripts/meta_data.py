05a62038b57f82261607e4b90dd2796687c0c9fa
Hoang Phan <phan.minhhoang2606@gmail.com>
2019-02-24 11:08:10 -0500

    Added requirements and test

--- scripts/meta_data.py
+++ scripts/meta_data.py
@@ -13,5 +13,13 @@ def predefined_data():
             {'start_time': 130, 'caption': 'River and tree on both sides of the river'},
             {'start_time': 144, 'caption': 'Sky and cloud and sun in the afternoon cloud flowing float moving move'},
             {'start_time': 163, 'caption': 'Lighhouse at noon mountain sea'}
+        ],
+        'QGJuMBdaqIw': [
+            {'start_time': 33,
+                'caption': 'I think it\'s a group of people playing at the pool'},
+            {'start_time': 81, 'caption': 'I think it\'s a group of people dancing'},
+            {'start_time': 94, 'caption': 'I think it\'s a person riding a skateboard'},
+            {'start_time': 182, 'caption':  'I think it\'s a group of people running'},
+            {'start_time': 195, 'caption':  'I think it\'s a girl standing under firework'}
         ]
     }
